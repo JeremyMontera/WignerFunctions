71f98d4f434dd5557faf38764012410f8586129f
Jeremy Montera <wittkopp.jeremy@gmail.com>
2023-09-29 19:29:57 -0700

    Updated ScalarPF class with major PF constructor.

--- src/WDF/PlenopticField/scalar_plenoptic_field.py
+++ src/WDF/PlenopticField/scalar_plenoptic_field.py
@@ -4,6 +4,7 @@ quantity.
 """
 
 import numpy as np
+from typing import Optional
 
 
 class PFError(Exception):
@@ -11,33 +12,72 @@ class PFError(Exception):
 
 
 class ScalarPF:
-    def __init__(self, arr: np.ndarray, pf_type: str, dim: str):
+    def __init__(
+            self, arr: np.ndarray, pf_type: str, dim: str, num: int,
+    ):
         """
         Constructor...
+
+        NOTE: this is explicitly called when constructing new scalar PFs representing 
+        coordinate arrays, coordinate vectors, etc.
 
         Args:
             arr:
                 Array from which the scalar PF will be constructed from. The user
-                should not have access to this object.
+                should not have access to this object. Size: $N_s \times 1$.
             pf_type:
                 The type of PF: major or minor. A major PF is a PF where the indices of
                 each micro-image correspond to the coordinates of the `arr`.
+            dim:
+                Direction that the corresponding coordinate varies in (either `'x'` for
+                the `x`-direction, or `'y'` for the `y`-direction).
+            num:
+                Either the number of micro-images to generate for each major coordinate
+                direction (assuming a minor PF) or the number pixels in each 
+                micro-image (assuming a major PF). It is assumed that all micro-images
+                and PFs are square.
         """
 
-        ...
+        if (pf_type.lower() == "major"):
+            self._construct_major(arr, num, dim)
 
-    def _construct_major(self, arr: np.ndarray) -> np.ndarray:
+    def _construct_major(
+            self, arr: np.ndarray, num_pixels: int, dim: int
+        ) -> None:
         """
         This private function constructs a major PF from the provided array.
 
         Args:
             arr:
-                ...
-
-        Returns:
-            ret:
-                ...
+                Array from which the scalar PF will be constructed from. The user
+                should not have access to this object. Size: $N_s \times 1$.
+            dim:
+                Direction that the corresponding coordinate varies in (either `'x'` for
+                the `x`-direction, or `'y'` for the `y`-direction).
+            num_pixels:
+                The number pixels in each micro-image. It is assumed that all 
+                micro-images and PFs are square.
         """
 
-        self._pf: np.ndarray = np.zeros((1, 1))
+        arr = arr.flatten()
+        num_micro: int = arr.shape[0]
+        self._pf: np.ndarray = np.zeros(
+            (num_micro * num_pixels, num_micro * num_pixels)
+        )
+
+        for i in range(num_micro):
+            qi = num_pixels * i
+
+            for j in range(num_micro):
+                qj = num_pixels * j
+
+                if dim.lower() == "x":
+                    self._pf[
+                        qi: qi + num_pixels, qj: qj + num_pixels
+                    ] = arr[j] * np.ones((num_pixels, num_pixels))
+                elif dim.lower() == "y":
+                    self._pf[
+                        qi: qi + num_pixels, qj: qj + num_pixels
+                    ] = arr[i] * np.ones((num_pixels))
+
         return self._pf
---
+++ tests/PlenopticField/test_scalar_pf.py
@@ -0,0 +1,1 @@
+from WDF.PlenopticField import ScalarPF
