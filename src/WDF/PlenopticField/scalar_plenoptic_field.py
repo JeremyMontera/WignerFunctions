e2ff82c4a108dc3878cd8a8267ff24effddbceee
Jeremy Montera <wittkopp.jeremy@gmail.com>
2023-10-06 16:00:32 -0700

    Updated ScalarPF class with optional constructors; updated unit tests

--- src/WDF/PlenopticField/scalar_plenoptic_field.py
+++ src/WDF/PlenopticField/scalar_plenoptic_field.py
@@ -3,7 +3,10 @@ This represents a "scalar" plenoptic field (PF). This is a PF-representation of 
 quantity.
 """
 
+from __future__ import annotations
+
 import numpy as np
+from typing import Optional, Tuple
 
 
 class PFError(Exception):
@@ -13,10 +16,10 @@ class PFError(Exception):
 class ScalarPF:
     def __init__(
         self,
-        arr: np.ndarray,
-        pf_type: str,
-        dim: str,
-        num: int,
+        arr: Optional[np.ndarray] = [],
+        pf_type: Optional[str] = "",
+        dim: Optional[str] = "",
+        num: Optional[int] = -1,
     ):
         """
         Constructor...
@@ -27,7 +30,9 @@ class ScalarPF:
         Args:
             arr:
                 Array from which the scalar PF will be constructed from. The user
-                should not have access to this object. Size: $N_s \times 1$.
+                should not have access to this object. Size: $N_s \times 1$. If the 
+                length of the array is zero, then the user is trying to build from 
+                another plenoptic field.
             pf_type:
                 The type of PF: major or minor. A major PF is a PF where the indices of
                 each micro-image correspond to the coordinates of the `arr`.
@@ -41,10 +46,60 @@ class ScalarPF:
                 and PFs are square.
         """
 
-        if pf_type.lower() == "major":
-            self._construct_major(arr, num, dim)
-        elif pf_type.lower() == "minor":
-            self._construct_minor(arr, num, dim)
+        if len(arr) > 0:
+            if pf_type.lower() == "major":
+                self._construct_major(arr, num, dim)
+            elif pf_type.lower() == "minor":
+                self._construct_minor(arr, num, dim)
+            else:
+                raise PFError(
+                    "***ERROR***:\tThe plenoptic field type entered is invalid\n" \
+                    f"pf_type = {pf_type}"
+                )
+        else:
+            if not pf_type == "" or not dim == "" or not num == -1:
+                raise PFError(
+                    "***ERROR***:\tYou have passed invalid arguments for initializing" \
+                    f" an empty plenoptic field: pf_type = {pf_type}, dim = {dim}," \
+                    f" num = {num}"
+                )
+            
+            self._pf: np.ndarray = np.array([])
+
+    def __add__(self, other: ScalarPF) -> ScalarPF:
+        """
+        Overrides Python's built-in "add" operator. This will add two scalar PFs on a
+        micro-image pixel-basis.
+
+        Args:
+            other:
+                The scalar PF to add this one to.
+
+        Returns:
+            return:
+                ...
+        """
+
+        ret: np.ndarray = self._pf + other._pf
+        return ScalarPF(arr=ret)
+    
+
+    def __sub__(self, other: ScalarPF) -> ScalarPF:
+        """
+        Overrides Python's built-in "minus" operator. This will add two scalar PFs on a
+        micro-image pixel-basis.
+
+        Args:
+            other:
+                The scalar PF to add this one to.
+
+        Returns:
+            return:
+                ...
+        """
+
+        ret: np.ndarray = self._pf - other._pf
+        return ScalarPF(arr=ret)
 
     def _construct_major(self, arr: np.ndarray, num_pixels: int, dim: int) -> None:
         """
@@ -60,6 +115,21 @@ class ScalarPF:
             dim:
                 Direction that the corresponding coordinate varies in (either `'x'` for
                 the `x`-direction, or `'y'` for the `y`-direction).
+
+        Raises:
+            PFError:
+                You passed a dimension not recognized
+                dimension = <...>
+
+        Example:
+            ```py
+            >>> import WDF.PlenopticField
+            >>> import numpy as np
+            >>> arr = np.array([0.0, 0.5, 1.0, 1.5, 2.0])
+            >>> major_pf = WDF.PlenopticField.ScalarPF(arr, "major", "x", 2)
+            >>> major_pf.shape
+            (10, 10)
+            ```
         """
 
         arr = arr.flatten()
@@ -82,6 +152,11 @@ class ScalarPF:
                     self._pf[qi : qi + num_pixels, qj : qj + num_pixels] = arr[
                         i
                     ] * np.ones((num_pixels))
+                else:
+                    raise PFError(
+                        "***ERROR***:\tYou passed a dimension not recognized\n" \
+                        f"dimension = {dim.lower()}"
+                    )
 
     def _construct_minor(self, arr: np.ndarray, num_micro: int, dim: int) -> None:
         """
@@ -97,6 +172,21 @@ class ScalarPF:
             dim:
                 Direction that the corresponding coordinate varies in (either `'x'` for
                 the `x`-direction, or `'y'` for the `y`-direction).
+
+        Raise:
+            PFError:
+                You passed a dimension not recognized
+                dimension = <...>
+
+        Example:
+            ```py
+            >>> import WDF.PlenopticField
+            >>> import numpy as np
+            >>> arr = np.array([0.0, 0.5, 1.0, 1.5, 2.0])
+            >>> minor_pf = WDF.PlenopticField.ScalarPF(arr, "minor", "x", 2)
+            >>> minor_pf.shape
+            (10, 10)
+            ```
         """
 
         arr = arr.flatten()
@@ -116,3 +206,21 @@ class ScalarPF:
                     self._pf[qi : qi + num_pixels, qj : qj + num_pixels] = arr_x
                 elif dim.lower() == "y":
                     self._pf[qi : qi + num_pixels, qj : qj + num_pixels] = arr_y
+                else:
+                    raise PFError(
+                        "***ERROR***:\tYou passed a dimension not recognized\n" \
+                        f"dimension = {dim.lower()}"
+                    )
+
+    @property
+    def shape(self) -> Tuple[int, int]:
+        """
+        Gets the shape of the plenoptic field: (number of micro-images * number of 
+        pixels per micro-image). This is just forwarding `numpy.shape` from the 
+        underlying array.
+
+        Type:
+            Tuple[int, int]
+        """
+
+        return self._pf.shape
--- tests/PlenopticField/test_scalar_pf.py
+++ tests/PlenopticField/test_scalar_pf.py
@@ -4,6 +4,7 @@ import numpy as np
 import pytest
 
 from WDF.PlenopticField import ScalarPF
+from WDF.PlenopticField.scalar_plenoptic_field import PFError
 
 
 @pytest.fixture
@@ -15,8 +16,8 @@ def setup_major_PFs():
     y = np.array([[-3, -1.5, 0, 1.5, 3]])
 
     # Major plenoptic fields
-    X = ScalarPF(x, "major", "x", 3)
-    Y = ScalarPF(y, "major", "y", 2)
+    X = ScalarPF(arr=x, pf_type="major", dim="x", num=3)
+    Y = ScalarPF(arr=y, pf_type="major", dim="y", num=2)
     return X._pf, Y._pf
 
 
@@ -106,7 +107,26 @@ def setup_minor_results():
     return X, Y
 
 
-def test_major_PF(setup_major_PFs, setup_major_results):
+def test_pf_empty_PF():
+    """Test the construction of an empty plenoptic field."""
+
+    empty = ScalarPF(arr=np.array([]))
+    assert empty._pf.shape == (0, )
+
+
+def test_pf_invalid_constructor():
+    """Test throwing error when trying to pass mutliple args."""
+
+    with pytest.raises(PFError) as exc:
+        ScalarPF(arr=np.array([]), dim="x")
+
+    assert exc.value.args[0] == (
+        "***ERROR***:\tYou have passed invalid arguments for initializing" \
+        " an empty plenoptic field: pf_type = , dim = x, num = -1"
+    )
+
+
+def test_pf_major_PF(setup_major_PFs, setup_major_results):
     """Test the construction of major plenoptic fields."""
 
     X_actual, Y_actual = setup_major_PFs
@@ -116,7 +136,7 @@ def test_major_PF(setup_major_PFs, setup_major_results):
     assert np.all(Y_actual == Y_result)
 
 
-def test_minor_PF(setup_minor_PFs, setup_minor_results):
+def test_pf_minor_PF(setup_minor_PFs, setup_minor_results):
     """Test the construction of minor plenoptic fields."""
 
     X_actual, Y_actual = setup_minor_PFs
@@ -124,3 +144,24 @@ def test_minor_PF(setup_minor_PFs, setup_minor_results):
 
     assert np.all(X_actual == X_result)
     assert np.all(Y_actual == Y_result)
+
+
+@pytest.mark.parametrize(
+        "pf_type",
+        [
+            "major", "minor"
+        ]
+)
+def test_pf_constructor_error_bad_dim(pf_type):
+    with pytest.raises(PFError) as exc:
+        ScalarPF(arr=np.zeros((1, 1)), pf_type=pf_type, dim="z", num=3)
+
+    assert exc.value.args[0] == \
+        "***ERROR***:\tYou passed a dimension not recognized\ndimension = z"
+    
+def test_pf_constructor_error_bad_type():
+    with pytest.raises(PFError) as exc:
+        ScalarPF(arr=np.zeros((1, 1)), pf_type="foo", dim="x", num=3)
+
+    assert exc.value.args[0] == \
+    "***ERROR***:\tThe plenoptic field type entered is invalid\npf_type = foo"
