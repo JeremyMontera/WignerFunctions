c20065be014d55575d4d2cfc666a49a4f1280a36
Jeremy Montera <wittkopp.jeremy@gmail.com>
2023-10-07 12:00:02 -0400

    RESTART

--- src/WDF/PlenopticField/scalar_plenoptic_field.py
+++ src/WDF/PlenopticField/scalar_plenoptic_field.py
@@ -5,321 +5,66 @@ quantity.
 
 from __future__ import annotations
 
+import dataclasses
+import enum
 from typing import Optional, Tuple
 
 import numpy as np
 
 
-TOL = 1E-10
+class SchemaValidator:
+
+    @dataclasses.dataclass
+    class Schema:
+        arr = np.ndarray
+        pf_type = str
+        dim = str
+        num = int
+        old_pf = np.ndarray
+
+    def validate(self, input_args: dict):
+        check = self.Schema()
+        for key in input_args.keys():
+            if hasattr(check, key):
+                assert isinstance(input_args[key], getattr(check, key)), \
+                    "Given input doesn't have the required type!"
+            else:
+                raise ValueError(f"You inputted an unexpected argument {key}!")
 
 
-class PFError(Exception):
-    ...
+class BuildOpt(enum.Enum):
+    New: int = 0
+    Old: int = 1
 
 
 class ScalarPF:
-    def __init__(
-        self,
-        arr: Optional[np.ndarray] = [],
-        pf_type: Optional[str] = "",
-        dim: Optional[str] = "",
-        num: Optional[int] = -1,
-    ):
-        """
-        Constructor...
 
-        NOTE: this is explicitly called when constructing new scalar PFs representing
-        coordinate arrays, coordinate vectors, etc.
+    def __init__(self, option: BuildOpt, **kwargs: dict):
+        SchemaValidator().validate(kwargs)
+        if option == BuildOpt.New:
+            self._construct_new(**kwargs)
+        elif option == BuildOpt.Old:
+            old_pf: np.ndarray = kwargs["old_pf"]
+            self._construct_old(old_pf)
 
-        Args:
-            arr:
-                Array from which the scalar PF will be constructed from. The user
-                should not have access to this object. Size: $N_s \times 1$. If the
-                length of the array is zero, then the user is trying to build from
-                another plenoptic field.
-            pf_type:
-                The type of PF: major or minor. A major PF is a PF where the indices of
-                each micro-image correspond to the coordinates of the `arr`.
-            dim:
-                Direction that the corresponding coordinate varies in (either `'x'` for
-                the `x`-direction, or `'y'` for the `y`-direction).
-            num:
-                Either the number of micro-images to generate for each major coordinate
-                direction (assuming a minor PF) or the number pixels in each
-                micro-image (assuming a major PF). It is assumed that all micro-images
-                and PFs are square.
-        """
+    def _construct_new(self, **kwargs: dict) -> None:
+        assert "arr" in kwargs.keys(), "You didn't provide an array to build from!"
+        assert "dim" in kwargs.keys(), "You didn't provide a dimension of the PF!"
+        assert "num" in kwargs.keys(), "You didn't provide a number to build the PF!"
 
-        if len(arr) > 0:
-            if pf_type.lower() == "major":
-                self._construct_major(arr, num, dim)
-            elif pf_type.lower() == "minor":
-                self._construct_minor(arr, num, dim)
-            else:
-                raise PFError(
-                    "***ERROR***:\tThe plenoptic field type entered is invalid\n"
-                    f"pf_type = {pf_type}"
-                )
+        arr, dim, num = kwargs["arr"], kwargs["dim"], kwargs["num"]
+        if kwargs["pf_type"].lower() == "major":
+            self._construct_new_major(arr, dim, num)
+        elif kwargs["pf_type"].lower() == "minor":
+            self._construct_new_minor(arr, dim, num)
         else:
-            if not pf_type == "" or not dim == "" or not num == -1:
-                raise PFError(
-                    "***ERROR***:\tYou have passed invalid arguments for initializing"
-                    f" an empty plenoptic field: pf_type = {pf_type}, dim = {dim},"
-                    f" num = {num}"
-                )
+            raise ValueError(f"You passed an invalid PF type {kwargs['pf_type']}!")
 
-            self._pf: np.ndarray = np.array([])
+    def _construct_new_major(self, arr: np.ndarray, dim: str, num: int) -> None:
+        ...
 
-    def __add__(self, other: ScalarPF) -> ScalarPF:
-        """
-        Overrides Python's built-in "add" operator. This will add two scalar PFs on a
-        micro-image pixel-basis.
+    def _construct_new_minor(self, arr: np.ndarray, dim: str, num: int) -> None:
+        ...
 
-        Args:
-            other:
-                The scalar PF to add this one to.
-
-        Returns:
-            return:
-                ...
-        """
-
-        if self.shape[0] != other.shape[0] and self.shape[1] != other.shape[1]:
-            raise PFError(
-                "***ERROR***:\tThe shapes of the two PFs are not the same."
-            )
-        
-        ret: np.ndarray = self._pf + other._pf
-        return ScalarPF(arr=ret)
-    
-
-    def __eq__(self, other: ScalarPF) -> bool:
-        """
-        Overrides Python's built-in comparison operator. This will check if two PFs are
-        equal (within some tolerance).
-
-        Args:
-            other:
-                ...
-
-        Returns:
-            flag:
-                ...
-        """
-
-        if self.shape[0] != other.shape[0] and self.shape[1] != other.shape[1]:
-            raise PFError(
-                "***ERROR***:\tThe shapes of the two PFs are not the same."
-            )
-        
-        assert np.all(abs(self._pf - other._pf) < TOL)
-    
-
-    def __truediv__(self, scalar: float) -> ScalarPF:
-        """
-        Overrides Python's built-in "divide" operator. This will divide the PF by a
-        scalar value and return the scaled PF.
-
-        Args:
-            scalar:
-                ...
-
-        Return:
-            return:
-                ...
-        """
-
-        if abs(scalar) < TOL:
-            raise ValueError("***ERROR***:\tYou are trying to divide by zero!")
-        
-        ret: np.ndarray = self._pf / scalar
-        return ScalarPF(arr=ret)
-    
-
-    def __mul__(self, scalar: float) -> ScalarPF:
-        """
-        Overrides Python's built-in "multiply" operator. This will multiply the PF by a
-        scalar value and return the scaled PF.
-
-        NOTE: this is for multiplcation on the left of the form
-        $\text{scalar} \times PF$.
-
-        Args:
-            scalar:
-                ...
-
-        Returns:
-            return:
-                ...
-        """
-
-        ret: np.ndarray = scalar * self._pf
-        return ScalarPF(arr=ret)
-    
-
-    def __rmul__(self, scalar: float) -> ScalarPF:
-        """
-        Overrides Python's built-in "multiply" operator. This will multiply the PF by a
-        scalar value and return the scaled PF.
-
-        NOTE: this is for multiplcation on the right of the form
-        $PF \times \text{scalar}$.
-
-        Args:
-            scalar:
-                ...
-
-        Returns:
-            return:
-                ...
-        """
-
-        ret: np.ndarray = scalar * self._pf
-        return ScalarPF(arr=ret)
-    
-
-    def __sub__(self, other: ScalarPF) -> ScalarPF:
-        """
-        Overrides Python's built-in "minus" operator. This will add two scalar PFs on a
-        micro-image pixel-basis.
-
-        Args:
-            other:
-                The scalar PF to add this one to.
-
-        Returns:
-            return:
-                ...
-        """
-
-        if self.shape[0] != other.shape[0] and self.shape[1] != other.shape[1]:
-            raise PFError(
-                "***ERROR***:\tThe shapes of the two PFs are not the same."
-            )
-
-        ret: np.ndarray = self._pf - other._pf
-        return ScalarPF(arr=ret)
-
-    def _construct_major(self, arr: np.ndarray, num_pixels: int, dim: int) -> None:
-        """
-        This private function constructs a major PF from the provided array.
-
-        Args:
-            arr:
-                Array from which the scalar PF will be constructed from. Size:
-                $N_s \times 1$.
-            num_pixels:
-                The number pixels in each micro-image. It is assumed that all
-                micro-images and PFs are square.
-            dim:
-                Direction that the corresponding coordinate varies in (either `'x'` for
-                the `x`-direction, or `'y'` for the `y`-direction).
-
-        Raises:
-            PFError:
-                You passed a dimension not recognized
-                dimension = <...>
-
-        Example:
-            ```py
-            >>> import WDF.PlenopticField
-            >>> import numpy as np
-            >>> arr = np.array([0.0, 0.5, 1.0, 1.5, 2.0])
-            >>> major_pf = WDF.PlenopticField.ScalarPF(arr, "major", "x", 2)
-            >>> major_pf.shape
-            (10, 10)
-            ```
-        """
-
-        arr = arr.flatten()
-        num_micro: int = arr.shape[0]
-        self._pf: np.ndarray = np.zeros(
-            (num_micro * num_pixels, num_micro * num_pixels)
-        )
-
-        for i in range(num_micro):
-            qi = num_pixels * i
-
-            for j in range(num_micro):
-                qj = num_pixels * j
-
-                if dim.lower() == "x":
-                    self._pf[qi : qi + num_pixels, qj : qj + num_pixels] = arr[
-                        j
-                    ] * np.ones((num_pixels, num_pixels))
-                elif dim.lower() == "y":
-                    self._pf[qi : qi + num_pixels, qj : qj + num_pixels] = arr[
-                        i
-                    ] * np.ones((num_pixels))
-                else:
-                    raise PFError(
-                        "***ERROR***:\tYou passed a dimension not recognized\n"
-                        f"dimension = {dim.lower()}"
-                    )
-
-    def _construct_minor(self, arr: np.ndarray, num_micro: int, dim: int) -> None:
-        """
-        This private function constructs a minor PF from the provided array.
-
-        Args:
-            arr:
-                Array from which the scalar PF will be constructed from. Size:
-                $N_s \times 1$.
-            num_micro:
-                The number pixels in each micro-image. It is assumed that all
-                micro-images and PFs are square.
-            dim:
-                Direction that the corresponding coordinate varies in (either `'x'` for
-                the `x`-direction, or `'y'` for the `y`-direction).
-
-        Raise:
-            PFError:
-                You passed a dimension not recognized
-                dimension = <...>
-
-        Example:
-            ```py
-            >>> import WDF.PlenopticField
-            >>> import numpy as np
-            >>> arr = np.array([0.0, 0.5, 1.0, 1.5, 2.0])
-            >>> minor_pf = WDF.PlenopticField.ScalarPF(arr, "minor", "x", 2)
-            >>> minor_pf.shape
-            (10, 10)
-            ```
-        """
-
-        arr = arr.flatten()
-        arr_x, arr_y = np.meshgrid(arr, arr)
-        num_pixels: int = arr.shape[0]
-        self._pf: np.ndarray = np.zeros(
-            (num_micro * num_pixels, num_micro * num_pixels)
-        )
-
-        for i in range(num_micro):
-            qi = num_pixels * i
-
-            for j in range(num_micro):
-                qj = num_pixels * j
-
-                if dim.lower() == "x":
-                    self._pf[qi : qi + num_pixels, qj : qj + num_pixels] = arr_x
-                elif dim.lower() == "y":
-                    self._pf[qi : qi + num_pixels, qj : qj + num_pixels] = arr_y
-                else:
-                    raise PFError(
-                        "***ERROR***:\tYou passed a dimension not recognized\n"
-                        f"dimension = {dim.lower()}"
-                    )
-
-    @property
-    def shape(self) -> Tuple[int, int]:
-        """
-        Gets the shape of the plenoptic field: (number of micro-images * number of
-        pixels per micro-image). This is just forwarding `numpy.shape` from the
-        underlying array.
-
-        Type:
-            Tuple[int, int]
-        """
-
-        return self._pf.shape
+    def _construct_old(self, old_pf: np.ndarray) -> None:
+        ...
--- tests/PlenopticField/test_scalar_pf.py
+++ tests/PlenopticField/test_scalar_pf.py
@@ -3,299 +3,46 @@
 import numpy as np
 import pytest
 
-from WDF.PlenopticField import ScalarPF
-from WDF.PlenopticField.scalar_plenoptic_field import PFError
-
-
-BINOPS = {"add": lambda a, b: a + b, "sub": lambda a, b: a - b}
-
-
-@pytest.fixture
-def setup_major_PFs():
-    """Setup major PFs in the $x$- and $y$-directions."""
-
-    # Initial coordinate arrays
-    x = np.array([[-1, 0, 1]])
-    y = np.array([[-3, -1.5, 0, 1.5, 3]])
-
-    # Major plenoptic fields
-    X = ScalarPF(arr=x, pf_type="major", dim="x", num=3)
-    Y = ScalarPF(arr=y, pf_type="major", dim="y", num=2)
-    return X._pf, Y._pf
-
-
-@pytest.fixture
-def setup_minor_PFs():
-    """Setup minor PFs in the $x$- and $y$-directions."""
-
-    # Initial coordinate arrays
-    x = np.array([[-1, 0, 1]])
-    y = np.array([[-3, -1.5, 0, 1.5, 3]])
-
-    # Major plenoptic fields
-    X = ScalarPF(x, "minor", "x", 3)
-    Y = ScalarPF(y, "minor", "y", 2)
-    return X._pf, Y._pf
-
-
-@pytest.fixture
-def setup_major_results():
-    """Setup what we expect the results should be for major PFs."""
-
-    X = np.array(
-        [
-            [-1, -1, -1, 0, 0, 0, 1, 1, 1],
-            [-1, -1, -1, 0, 0, 0, 1, 1, 1],
-            [-1, -1, -1, 0, 0, 0, 1, 1, 1],
-            [-1, -1, -1, 0, 0, 0, 1, 1, 1],
-            [-1, -1, -1, 0, 0, 0, 1, 1, 1],
-            [-1, -1, -1, 0, 0, 0, 1, 1, 1],
-            [-1, -1, -1, 0, 0, 0, 1, 1, 1],
-            [-1, -1, -1, 0, 0, 0, 1, 1, 1],
-            [-1, -1, -1, 0, 0, 0, 1, 1, 1],
-        ]
-    )
-
-    Y = np.array(
-        [
-            [-3, -3, -3, -3, -3, -3, -3, -3, -3, -3],
-            [-3, -3, -3, -3, -3, -3, -3, -3, -3, -3],
-            [-1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5],
-            [-1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5],
-            [1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5],
-            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-        ]
-    )
-
-    return X, Y
-
-
-@pytest.fixture
-def setup_minor_results():
-    """Setup what we expect the results should be for minor PFs."""
-
-    X = np.array(
-        [
-            [-1, 0, 1, -1, 0, 1, -1, 0, 1],
-            [-1, 0, 1, -1, 0, 1, -1, 0, 1],
-            [-1, 0, 1, -1, 0, 1, -1, 0, 1],
-            [-1, 0, 1, -1, 0, 1, -1, 0, 1],
-            [-1, 0, 1, -1, 0, 1, -1, 0, 1],
-            [-1, 0, 1, -1, 0, 1, -1, 0, 1],
-            [-1, 0, 1, -1, 0, 1, -1, 0, 1],
-            [-1, 0, 1, -1, 0, 1, -1, 0, 1],
-            [-1, 0, 1, -1, 0, 1, -1, 0, 1],
-        ]
-    )
-
-    Y = np.array(
-        [
-            [-3, -3, -3, -3, -3, -3, -3, -3, -3, -3],
-            [-1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5],
-            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-            [-3, -3, -3, -3, -3, -3, -3, -3, -3, -3],
-            [-1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5],
-            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-        ]
-    )
-
-    return X, Y
-
-
-def test_pf_empty_PF():
-    """Test the construction of an empty plenoptic field."""
-
-    empty = ScalarPF(arr=np.array([]))
-    assert empty._pf.shape == (0,)
-
-
-def test_pf_invalid_constructor():
-    """Test throwing error when trying to pass mutliple args."""
-
-    with pytest.raises(PFError) as exc:
-        ScalarPF(arr=np.array([]), dim="x")
-
-    assert exc.value.args[0] == (
-        "***ERROR***:\tYou have passed invalid arguments for initializing"
-        " an empty plenoptic field: pf_type = , dim = x, num = -1"
-    )
-
-
-def test_pf_major_PF(setup_major_PFs, setup_major_results):
-    """Test the construction of major plenoptic fields."""
-
-    X_actual, Y_actual = setup_major_PFs
-    X_result, Y_result = setup_major_results
-
-    assert np.all(X_actual == X_result)
-    assert np.all(Y_actual == Y_result)
-
-
-def test_pf_minor_PF(setup_minor_PFs, setup_minor_results):
-    """Test the construction of minor plenoptic fields."""
-
-    X_actual, Y_actual = setup_minor_PFs
-    X_result, Y_result = setup_minor_results
-
-    assert np.all(X_actual == X_result)
-    assert np.all(Y_actual == Y_result)
-
-
-@pytest.mark.parametrize("pf_type", ["major", "minor"])
-def test_pf_constructor_error_bad_dim(pf_type):
-    """Tests that an error is raised when passing an unknown dimension."""
-
-    with pytest.raises(PFError) as exc:
-        ScalarPF(arr=np.zeros((1, 1)), pf_type=pf_type, dim="z", num=3)
-
-    assert (
-        exc.value.args[0]
-        == "***ERROR***:\tYou passed a dimension not recognized\ndimension = z" # noqa: W503
-    )
-
-
-def test_pf_constructor_error_bad_type():
-    """Tests that an error is raised when passing an unknown pf type."""
-
-    with pytest.raises(PFError) as exc:
-        ScalarPF(arr=np.zeros((1, 1)), pf_type="foo", dim="x", num=3)
-
-    assert (
-        exc.value.args[0]
-        == "***ERROR***:\tThe plenoptic field type entered is invalid\npf_type = foo" # noqa: W503
-    )
-
-
-@pytest.fixture
-def add(setup_major_PFs, setup_minor_PFs):
-    X_major, Y_major = setup_major_PFs
-    X_minor, Y_minor = setup_minor_PFs
-
-    ret_X: ScalarPF = X_major + X_minor
-    ret_Y: ScalarPF = Y_major + Y_minor
-
-    return ret_X, ret_Y
-
-
-@pytest.fixture
-def add_res():
-    ret_X: np.ndarray = np.array(
-        [
-            [-2, -1, 0, -1, 0, 1, 0, 1, 2],
-            [-2, -1, 0, -1, 0, 1, 0, 1, 2],
-            [-2, -1, 0, -1, 0, 1, 0, 1, 2],
-            [-2, -1, 0, -1, 0, 1, 0, 1, 2],
-            [-2, -1, 0, -1, 0, 1, 0, 1, 2],
-            [-2, -1, 0, -1, 0, 1, 0, 1, 2],
-            [-2, -1, 0, -1, 0, 1, 0, 1, 2],
-            [-2, -1, 0, -1, 0, 1, 0, 1, 2],
-            [-2, -1, 0, -1, 0, 1, 0, 1, 2],
-        ]
-    )
-
-    ret_Y: np.ndarray = np.array(
-        [
-            [-6, -6, -6, -6, -6, -6, -6, -6, -6, -6],
-            [-4.5, -4.5, -4.5, -4.5, -4.5, -4.5, -4.5, -4.5, -4.5, -4.5],
-            [-1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-            [-3, -3, -3, -3, -3, -3, -3, -3, -3, -3],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5],
-            [4.5, 4.5, 4.5, 4.5, 4.5, 4.5, 4.5, 4.5, 4.5, 4.5],
-            [6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-        ]
-    )
-
-    return ret_X, ret_Y
-
-
-@pytest.fixture
-def sub(setup_major_PFs, setup_minor_PFs):
-    X_major, Y_major = setup_major_PFs
-    X_minor, Y_minor = setup_minor_PFs
-
-    ret_X: ScalarPF = X_major + X_minor
-    ret_Y: ScalarPF = Y_major - Y_minor
-
-    return ret_X, ret_Y
-
-
-@pytest.fixture
-def sub_res():
-    ret_X: np.ndarray = np.array(
-        [
-            [-2, -1, 0, -1, 0, 1, 0, 1, 2],
-            [-2, -1, 0, -1, 0, 1, 0, 1, 2],
-            [-2, -1, 0, -1, 0, 1, 0, 1, 2],
-            [-2, -1, 0, -1, 0, 1, 0, 1, 2],
-            [-2, -1, 0, -1, 0, 1, 0, 1, 2],
-            [-2, -1, 0, -1, 0, 1, 0, 1, 2],
-            [-2, -1, 0, -1, 0, 1, 0, 1, 2],
-            [-2, -1, 0, -1, 0, 1, 0, 1, 2],
-            [-2, -1, 0, -1, 0, 1, 0, 1, 2],
-        ]
-    )
-
-    ret_Y: np.ndarray = np.array(
-        [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [-1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5],
-            [-1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5, -1.5],
-            [-3, -3, -3, -3, -3, -3, -3, -3, -3, -3],
-            [-3, -3, -3, -3, -3, -3, -3, -3, -3, -3],
-            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-            [1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5],
-            [1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        ]
-    )
-
-    return ret_X, ret_Y
-
-
-@pytest.mark.parametrize(
-    ("binop", "result"),
-    [
-        ("add", "add_res"),
-        ("sub", "sub_res"),
-    ]
+from WDF.PlenopticField.scalar_plenoptic_field import (
+    BuildOpt, SchemaValidator, ScalarPF
 )
-def test_pf_pf_operators(binop, result, request):
-    """Tests binary operations of plenoptic fields: addition/subtraction."""
-
-    binop = request.getfixturevalue(binop)
-    result = request.getfixturevalue(result)
-
-    assert np.all(binop[0] == result[0])
-    assert np.all(binop[1] == result[1])
 
 
-@pytest.mark.parametrize(
-    ("binop", ),
-    [
-        ("add", ),
-        ("sub", ),
-    ]
-)
-def test_pf_pf_operators_error_shape(binop):
-    """Tests error thrown when trying to add/subtract two different shapes."""
+def test_scalar_pf_schema_validator_fails():
+    input_args = {
+        "arr": np.array([1, 2, 3]),
+        "dim": "x",
+        "num": 10.0,
+        "pf_type": "major"
+    }
 
-    x1: ScalarPF = ScalarPF(arr=np.array([0.0, 1.0]), pf_type="major", dim="x", num=2)
-    x2: ScalarPF = ScalarPF(arr=np.array([0.0, 1.0]), pf_type="minor", dim="y", num=3)
+    with pytest.raises(AssertionError) as exc:
+        SchemaValidator().validate(input_args)
 
-    with pytest.raises(PFError) as exc:
-        BINOPS[binop](x1, x2)
+    assert exc.value.args[0] == "Given input doesn't have the required type!"
 
-    assert exc.value.args[0] == \
-        "***ERROR***:\tThe shapes of the two PFs are not the same."
+    input_args["num"] = 10
+    input_args["foo"] = "bar"
+    with pytest.raises(ValueError) as exc:
+        SchemaValidator().validate(input_args)
+
+    assert exc.value.args[0] == "You inputted an unexpected argument foo!"
+
+
+def test_scalar_pf_factory_new_fails():
+    with pytest.raises(AssertionError) as exc:
+        ScalarPF(BuildOpt.New, pf_type="major", arr=np.array([0, 1, 2]), dim="x")
+
+    assert exc.value.args[0] == "You didn't provide a number to build the PF!"
+    with pytest.raises(AssertionError) as exc:
+        ScalarPF(BuildOpt.New, pf_type="major", arr=np.array([0, 1, 2]), num=10)
+
+    assert exc.value.args[0] == "You didn't provide a dimension of the PF!"
+    with pytest.raises(AssertionError) as exc:
+        ScalarPF(BuildOpt.New, pf_type="major", dim="x", num=10)
+
+    assert exc.value.args[0] == "You didn't provide an array to build from!"
+    with pytest.raises(ValueError) as exc:
+        ScalarPF(BuildOpt.New, pf_type="eggs", arr=np.array([0, 1, 2]), dim="x", num=10)
+
+    assert exc.value.args[0] == "You passed an invalid PF type eggs!"
